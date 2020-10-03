fac14de90a09ae36fa42dae09208b9129d8af8db
unknown <laurenalayzacodes@gmail.com>
2020-10-03 10:01:14 -0400

    auth is ready & app.tsx

---
+++ src/BookLists/BookListTable.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+// export interface BookListTableProps {
+
+// }
+
+// export interface BookListTableState {
+
+// }
+
+// class BookListTable extends React.Component<BookListTableProps, BookListTableState> {
+//     constructor(props: BookListTableProps) {
+//         super(props);
+//         this.state = { :  };
+//     }
+//     render() {
+//         return (  );
+//     }
+// }
+
+// export default BookListTable;
---
+++ src/BookLists/BooklistEdit.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+// export interface BookListEditProps {
+    
+// }
+ 
+// export interface BookListEditState {
+    
+// }
+ 
+// class BookListEdit extends React.Component<BookListEditProps, BookListEditState> {
+//     constructor(props: BookListEditProps) {
+//         super(props);
+//         this.state = { :  };
+//     }
+//     render() { 
+//         return (  );
+//     }
+// }
+ 
+// export default BookListEdit;
